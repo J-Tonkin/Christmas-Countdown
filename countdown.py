d8287b3b21cb0b156e39ad509485bbdf59c9fab8
J-Tonkin <85169566+J-Tonkin@users.noreply.github.com>
2021-11-25 23:13:42 +1030

    Add rotate method

--- countdown.py
+++ countdown.py
@@ -18,22 +18,40 @@ LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-def GetRow(row,disp):
+def GetRow(seg,disp):
     offset = disp*14
-    if (row == 0):
+    if (seg == 0):
         leds = [0,13]
-    if (row == 1):
+    if (seg == 1):
         leds = [1,12]
-    if (row == 2):
+    if (seg == 2):
         leds = [2,11]
-    if (row == 3):
+    if (seg == 3):
         leds = [3,10]
-    if (row == 4):
+    if (seg == 4):
         leds = [4,9]
-    if (row == 5):
+    if (seg == 5):
         leds = [5,8]
-    if (row == 6):
+    if (seg == 6):
         leds = [6,7]
+    return [z+offset for z in leds]
+    
+def GetPinwheel(seg,disp):
+    offset = disp*14
+    if (seg == 0):
+        leds = [6,13]
+    if (seg == 1):
+        leds = [5,12]
+    if (seg == 2):
+        leds = [4,11]
+    if (seg == 3):
+        leds = [3,10]
+    if (seg == 4):
+        leds = [2,9]
+    if (seg == 5):
+        leds = [1,8]
+    if (seg == 6):
+        leds = [0,7]
     return [z+offset for z in leds]
 
 def DigitToLed(num,disp):
@@ -77,19 +95,54 @@ def UpAndDown(r,g,b):
     prevRow = []
     for disp in range(math.floor(LED_COUNT/14)):
         prevRow = []
-        for row in range (7):
+        for seg in range (7):
             prevRow = leds
-            leds = GetRow(row,disp)
+            leds = GetRow(seg,disp)
             for led in leds:
                 strip.setPixelColor(led,Color(r,g,b))
-            if(row != 0):
+            if(seg != 0):
                 for led in prevRow:
                     strip.setPixelColor(led,Color(0,0,0))
             strip.show()
             time.sleep(.1)
-        for row in range (6,-1,-1):
+        for seg in range (6,-1,-1):
             prevRow = leds
-            leds = GetRow(row,disp)
+            leds = GetRow(seg,disp)
+            for led in leds:
+                strip.setPixelColor(led,Color(r,g,b))
+            for led in prevRow:
+                if (state[led]):
+                    strip.setPixelColor(led,Color(r,g,b))
+                else :
+                    strip.setPixelColor(led,Color(0,0,0))
+            strip.show()
+            time.sleep(.1)
+        prevRow = leds
+        for led in prevRow:
+            if (state[led]):
+                strip.setPixelColor(led,Color(r,g,b))
+            else :
+                strip.setPixelColor(led,Color(0,0,0))
+        strip.show()
+        
+def Rotate(r,g,b):
+    leds = []
+    prevRow = []
+    for disp in range(math.floor(LED_COUNT/14)):
+        prevRow = []
+        for seg in range (7):
+            prevRow = leds
+            leds = GetPinwheel(seg,disp)
+            for led in leds:
+                strip.setPixelColor(led,Color(r,g,b))
+            if(seg != 0):
+                for led in prevRow:
+                    strip.setPixelColor(led,Color(0,0,0))
+            strip.show()
+            time.sleep(.1)
+        for seg in range (6,-1,-1):
+            prevRow = leds
+            leds = GetPinwheel(seg,disp)
             for led in leds:
                 strip.setPixelColor(led,Color(r,g,b))
             for led in prevRow:
@@ -147,7 +200,7 @@ if __name__ == '__main__':
                 display = display + 1
             strip.show()
             time.sleep(60)
-            UpAndDown(255,0,0)
+            Rotate(255,0,0)
             time.sleep(5)
 
             now = dt.now()
